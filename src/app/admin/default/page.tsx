577b23dd18bb49605d1490447ddeccdc05f6f239
Liladhar Kumawat <liladharkumawat007@gmail.com>
2025-02-26 22:14:11 +0530

    add dasbodercharts/10.13/26

--- src/app/admin/default/page.tsx
+++ src/app/admin/default/page.tsx
@@ -1,14 +1,56 @@
 'use client';
-
-// import TaskCard from 'components/admin/default/TaskCard';
+import WeeklyRevenue from 'components/admin/default/WeeklyRevenue';
+import TotalSpent from 'components/admin/default/TotalSpent';
+import { IoMdHome } from 'react-icons/io';
+import { IoDocuments } from 'react-icons/io5';
+import { MdBarChart, MdDashboard } from 'react-icons/md';
+import Widget from 'components/widget/Widget';
 
 const Dashboard = () => {
   return (
-    <>
-      <div className="flex h-screen items-center justify-center">
-        <h1 className="text-3xl font-bold text-gray-900">Main Dashboard</h1>
+    <div>
+      {/* Card widget */}
+
+      <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
+        <Widget
+          icon={<MdBarChart className="h-7 w-7" />}
+          title={'Earnings'}
+          subtitle={'$340.5'}
+        />
+        <Widget
+          icon={<IoDocuments className="h-6 w-6" />}
+          title={'Spend this month'}
+          subtitle={'$642.39'}
+        />
+        <Widget
+          icon={<MdBarChart className="h-7 w-7" />}
+          title={'Sales'}
+          subtitle={'$574.34'}
+        />
+        <Widget
+          icon={<MdDashboard className="h-6 w-6" />}
+          title={'Your Balance'}
+          subtitle={'$1,000'}
+        />
+        <Widget
+          icon={<MdBarChart className="h-7 w-7" />}
+          title={'New Tasks'}
+          subtitle={'145'}
+        />
+        <Widget
+          icon={<IoMdHome className="h-6 w-6" />}
+          title={'Total Projects'}
+          subtitle={'$2433'}
+        />
       </div>
-    </>
+
+      {/* Charts */}
+
+      <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2">
+        <TotalSpent />
+        <WeeklyRevenue />
+      </div>
+    </div>
   );
 };
 
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="flex w-full flex-col items-center justify-between px-1 pb-8 pt-3 lg:px-8 xl:flex-row">
       <p className="mb-4 text-center text-sm font-medium text-gray-600 sm:!mb-0 md:text-lg">
         <span className="mb-4 text-center text-sm text-gray-600 sm:!mb-0 md:text-base">
-          ©{new Date().getFullYear()}IYF Jaipur. All Rights Reserved. Made with love by IYF Devotees!
+          ©{new Date().getFullYear()} IYF Jaipur. All Rights Reserved. Made with love by IYF Devotees!
         </span>
       </p>
  
