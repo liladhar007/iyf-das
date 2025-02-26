958825e6b20d81e88aa0483a4aac9d150de52e7c
Liladhar Kumawat <liladharkumawat007@gmail.com>
2025-02-26 14:39:49 +0530

    update2.39/26

--- src/components/allifycomponet/Allbatches/index.jsx
+++ src/components/allifycomponet/Allbatches/index.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+'use client';
 
-const Allbatches = () => {
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+const AllBatches = () => {
+  const router = useRouter();
+
+  const batches = [
+    { id: 1, name: 'DYS-1' },
+    { id: 2, name: 'DYS-2' },
+    { id: 3, name: 'DYS-3' },
+    { id: 4, name: 'DYS-4' },
+    { id: 5, name: 'DYS-5' },
+    { id: 6, name: 'DYS-6' },
+    { id: 7, name: 'DYS-7' },
+    { id: 8, name: 'Nachiketa' },
+    { id: 9, name: 'Sahdev' },
+    { id: 10, name: 'Nakul' },
+  ];
+
   return (
-    <div>
-      Allbatches
+    <div className="p-2 mt-5">
+      <div className="grid grid-cols-2 gap-4">
+        {batches.map((batch) => (
+          <div
+            key={batch.id}
+            className="p-4 border rounded-lg shadow cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 dark:border-gray-600 dark:bg-gray-900 dark:text-white"
+            onClick={() => router.push(`/admin/batches/${batch.name}`)}
+          >
+            {batch.name}
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Allbatches
+export default AllBatches;
